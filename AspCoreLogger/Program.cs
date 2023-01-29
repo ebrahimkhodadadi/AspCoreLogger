78d3f108f0ed4cb071a4b7b2557e2da578c30e1f
ebrahimkhodadadi <ebrahimkhodadadideveloper@gmail.com>
2023-01-29 22:11:55 +0330

    add elastic service extension

--- AspCoreLogger/Program.cs
+++ AspCoreLogger/Program.cs
@@ -1,4 +1,9 @@
 using AspCoreLogger.Middleware;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection.PortableExecutable;
 
 namespace AspCoreLogger
 {
@@ -15,6 +20,30 @@ namespace AspCoreLogger
             builder.Services.AddEndpointsApiExplorer();
             
             builder.Services.AddSwaggerGen();
+
+            #region serilog
+            builder.Host.UseContentRoot(Directory.GetCurrentDirectory());
+            builder.Host
+                .UseSerilog((hostingContext, config) => config
+                .Enrich.FromLogContext()
+                .Enrich.WithExceptionDetails()
+                .Enrich.WithMachineName()
+                .Enrich.WithCorrelationId()
+                .Enrich.WithProperty("Environment", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"))
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(ConfigureElasticSearchSink())
+                .ReadFrom.Configuration(hostingContext.Configuration));
+            
+            builder.Configuration
+                        .SetBasePath(builder.Environment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+                        .AddEnvironmentVariables();
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+            builder.Logging.AddSerilog();
+            #endregion
 
             var app = builder.Build();
 
@@ -33,6 +62,20 @@ namespace AspCoreLogger
             app.MapControllers();
 
             app.Run();
+        }
+
+        private static ElasticsearchSinkOptions ConfigureElasticSearchSink()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+                .Build();
+            var elasticUri = new Uri(configuration["ElasticConfiguration:Uri"]);
+            return new ElasticsearchSinkOptions(elasticUri)
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = $"logging-microservice-architecture-demo-{DateTime.UtcNow:yyyy-MM}"
+            };
         }
     }
 }
